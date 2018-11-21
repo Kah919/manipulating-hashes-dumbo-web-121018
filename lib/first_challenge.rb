c627a82ad7b604e7616d601818ae8e894a5983cc
Kah Yap <kahyap13@gmail.com>
2018-11-21 09:15:15 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,14 +13,14 @@ def first_challenge
     }
   }
 
-  # your code here
-  # contacts.each do |name, attribute|
-  #   attribute.each do |k, v|
-  #     if k == :favorite_icecream_flavors
-  #       v.delete_if { |x| x == "strawberry" }
-  #     end
-  #   end
-  # end
+  your code here
+  contacts.each do |name, attribute|
+    attribute.each do |k, v|
+      if k == :favorite_icecream_flavors
+        v.delete_if { |x| x == "strawberry" }
+      end
+    end
+  end
 
   #remember to return your newly altered contacts hash!
   contacts
