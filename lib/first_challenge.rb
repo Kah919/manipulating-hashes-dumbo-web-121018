ea8db719b62d40da9b88fccf42658568534e21ea
Kah Yap <kahyap13@gmail.com>
2018-11-21 09:08:10 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,12 +1,12 @@
 def first_challenge
   contacts = {
-    "Jon Snow" => {
+    Jon Snow: {
       name: "Jon",
       email: "jon_snow@thewall.we",
       favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
       knows: nil
     },
-    "Freddy Mercury" => {
+    Freddy Mercury: {
       name: "Freddy",
       email: "freddy@mercury.com",
       favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
